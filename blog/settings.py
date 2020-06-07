650b09fa0061b209485e96d0cde2d8825536afdd
MkDierz <msyibbran24@gmail.com>
2020-06-08 02:22:02 +0700

    memew

--- blog/settings.py
+++ blog/settings.py
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'tr'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'Europe/Istanbul'
 
