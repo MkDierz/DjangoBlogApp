8d054913ae1a77ff4348188151e91b520ab809c6
MkDierz <msyibbran24@gmail.com>
2020-06-08 00:25:54 +0700

    inital commit

---
+++ article/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import handler400, handler500
+from . import views
+
+app_name = "article"
+
+urlpatterns = [
+    path('dashboard/', views.dashboard, name="dashboard"),
+    path('addarticle/', views.addArticle, name="addarticle"),
+    path('article/<slug:slug>/', views.detail, name="detail"),
+    path('update/<slug:slug>', views.updateArticle, name="update"),
+    path('delete/<slug:slug>', views.deleteArticle, name="delete"),
+    path('', views.articles, name="articles"),
+    path('comment/<slug:slug>', views.addComment, name="comment"),
+
+]
